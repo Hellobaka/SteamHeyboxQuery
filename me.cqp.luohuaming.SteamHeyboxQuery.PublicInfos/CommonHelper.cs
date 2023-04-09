17099d61998f17f680dab5f03326046800896633
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-04-09 18:12:22 +0800

    修复解析错误

--- me.cqp.luohuaming.SteamHeyboxQuery.PublicInfos/CommonHelper.cs
+++ me.cqp.luohuaming.SteamHeyboxQuery.PublicInfos/CommonHelper.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using me.cqp.luohuaming.SteamHeyboxQuery.Sdk.Cqp.Model;
 using me.cqp.luohuaming.SteamHeyboxQuery.Tool.IniConfig;
@@ -42,7 +43,9 @@ namespace me.cqp.luohuaming.SteamHeyboxQuery.PublicInfos
         public static Dictionary<string, int> SteamIdCache { get; set; } = new Dictionary<string, int>();
         public static string DownloadString(string url) {
             using(var client = new System.Net.WebClient()) {
-                return client.DownloadString(url);
+                string json = client.DownloadString(url);
+                json = Regex.Replace(json, "heybox:\\/\\/.*\"", "\"");
+                return json;
             }
         }
     }
--- me.cqp.luohuaming.SteamHeyboxQuery.PublicInfos/Model/SearchResult.cs
+++ me.cqp.luohuaming.SteamHeyboxQuery.PublicInfos/Model/SearchResult.cs
@@ -10,120 +10,158 @@ namespace me.cqp.luohuaming.SteamHeyboxQuery.PublicInfos.Model
         public string version { get; set; }
         public Result result { get; set; }
 
-        public class CouponInfo
+        public class Result
         {
-            public string coupon_desc { get; set; }
-            public string title { get; set; }
-            public int available_coupon_num { get; set; }
-            public string sub_title { get; set; }
-            public int max_reduce { get; set; }
-            public int primary_id { get; set; }
-            public string coupon_protocol { get; set; }
-        }
-
-        public class HeyboxPrice
-        {
-            public CouponInfo coupon_info { get; set; }
-            public bool has_sl { get; set; }
-            public int is_lowest { get; set; }
-            public int discount { get; set; }
-            public int original_coin { get; set; }
-            public int cost_coin { get; set; }
-            public string end_time { get; set; }
-            public int? deadline_timestamp { get; set; }
-        }
-
-        public class Price
-        {
-            public string current { get; set; }
-            public int deadline_timestamp { get; set; }
-            public string initial { get; set; }
-            public int is_lowest { get; set; }
-            public int discount { get; set; }
-            public string deadline_date { get; set; }
-            public string lowest_price_raw { get; set; }
-            public float lowest_price { get; set; }
-            public float new_lowest { get; set; }
-            public float lowest_discount { get; set; }
-        }
-
-        public class Price2
-        {
-            public bool is_free { get; set; }
-            public int? is_lowest { get; set; }
-            public string region_name { get; set; }
-            public int? discount { get; set; }
-            public string region { get; set; }
-            public string initial { get; set; }
-            public string discount_desc { get; set; }
-            public string final_amount { get; set; }
-            public string current { get; set; }
-            public string initial_amount { get; set; }
-            public string deadline_date { get; set; }
-            public int? deadline_timestamp { get; set; }
-            public int? new_lowest { get; set; }
-            public int? plus_discount { get; set; }
-            public string lowest_price { get; set; }
-            public string image { get; set; }
-        }
-
-        public class PlatformInfo
-        {
-            public string img_url { get; set; }
-            public string key { get; set; }
-            public Price2 price { get; set; }
-        }
-
-        public class Info
-        {
-            public int steam_appid { get; set; }
-            public IList<object> genres { get; set; }
-            public string name { get; set; }
-            public string h_src { get; set; }
-            public IList<string> platforms_url { get; set; }
-            public string hidden_type { get; set; }
-            public string image { get; set; }
-            public string game_type { get; set; }
-            public bool is_free { get; set; }
-            public IList<string> platforms { get; set; }
-            public int is_owned { get; set; }
-            public string score { get; set; }
-            public IList<string> platforms_icon { get; set; }
-            public string appicon { get; set; }
-            public HeyboxPrice heybox_price { get; set; }
-            public string type { get; set; }
-            public Price price { get; set; }
-            public IList<PlatformInfo> platform_infos { get; set; }
-            public string is_bundle { get; set; }
-            public string score_desc { get; set; }
-            public bool? is_release { get; set; }
-            public string bundle_id { get; set; }
-            public int? hs_inventory { get; set; }
-            public int? online_player { get; set; }
-            public int? has_achievement { get; set; }
-            public string release_date { get; set; }
-            public string peak_user_num { get; set; }
+            public Item[] items { get; set; }
+            public object[] sort_filter_list { get; set; }
+            public Game_Type_List[] game_type_list { get; set; }
+            public object[] time_range_list { get; set; }
         }
 
         public class Item
         {
             public Info info { get; set; }
             public string type { get; set; }
+            public string weight { get; set; }
             public string report_id { get; set; }
         }
 
-        public class GameTypeList
+        public class Info
+        {
+            public int steam_appid { get; set; }
+            public string[] platforms_url { get; set; }
+            public string image { get; set; }
+            public string[] platforms { get; set; }
+            public Heybox_Price heybox_price { get; set; }
+            public object[] genres { get; set; }
+            public string hidden_type { get; set; }
+            public string score { get; set; }
+            public string appicon { get; set; }
+            public string type { get; set; }
+            public string h_src { get; set; }
+            public Price price { get; set; }
+            public bool is_free { get; set; }
+            public string name { get; set; }
+            public string game_type { get; set; }
+            public string[] platforms_icon { get; set; }
+            public int is_owned { get; set; }
+            public Platform_Infos[] platform_infos { get; set; }
+            public object is_bundle { get; set; }
+            public string name_en { get; set; }
+            public string score_desc { get; set; }
+            public string peak_user_num { get; set; }
+            public int online_player { get; set; }
+            public int has_achievement { get; set; }
+            public string[] free_types { get; set; }
+            public Rich_Tags[] rich_tags { get; set; }
+            public int hs_inventory { get; set; }
+            public string release_date { get; set; }
+            public int expect_num { get; set; }
+            public string protocol { get; set; }
+            public string vertical_img_bg { get; set; }
+            public string download_url_android { get; set; }
+            public bool is_release { get; set; }
+            public string download_url_ios { get; set; }
+            public string bundle_id { get; set; }
+        }
+
+        public class Heybox_Price
+        {
+            public bool has_sl { get; set; }
+            public int is_lowest { get; set; }
+            public int discount { get; set; }
+            public int original_coin { get; set; }
+            public int cost_coin { get; set; }
+            public object end_time { get; set; }
+        }
+
+        public class Price
+        {
+            public object current { get; set; }
+            public string initial { get; set; }
+            public int is_lowest { get; set; }
+            public int discount { get; set; }
+            public string lowest_price_raw { get; set; }
+            public object lowest_price { get; set; }
+            public int new_lowest { get; set; }
+            public int lowest_discount { get; set; }
+            public string current_v { get; set; }
+            public string region_name { get; set; }
+            public string initial_amount { get; set; }
+            public string region { get; set; }
+            public string final_amount { get; set; }
+            public string deadline_date { get; set; }
+            public int deadline_timestamp { get; set; }
+            public int plus_discount { get; set; }
+        }
+
+        public class Platform_Infos
+        {
+            public Price1 price { get; set; }
+            public string img_url { get; set; }
+            public string key { get; set; }
+        }
+
+        public class Price1
+        {
+            public int is_lowest { get; set; }
+            public string current_v { get; set; }
+            public string region_name { get; set; }
+            public string initial_amount { get; set; }
+            public string region { get; set; }
+            public string initial { get; set; }
+            public string deadline_date { get; set; }
+            public bool is_free { get; set; }
+            public string final_amount { get; set; }
+            public string current { get; set; }
+            public int discount { get; set; }
+            public string discount_desc { get; set; }
+            public object deadline_timestamp { get; set; }
+            public int new_lowest { get; set; }
+            public int plus_discount { get; set; }
+            public string lowest_price { get; set; }
+            public string image { get; set; }
+            public string purchase_protocol { get; set; }
+        }
+
+        public class Rich_Tags
+        {
+            public int text_alignment { get; set; }
+            public string border_color { get; set; }
+            public int height { get; set; }
+            public int corner_radius { get; set; }
+            public Inset inset { get; set; }
+            public Attr[] attrs { get; set; }
+            public string border_width { get; set; }
+            public string background_color { get; set; }
+        }
+
+        public class Inset
+        {
+            public int top { get; set; }
+            public int right { get; set; }
+            public int bottom { get; set; }
+            public int left { get; set; }
+        }
+
+        public class Attr
+        {
+            public int width { get; set; }
+            public string image { get; set; }
+            public string type { get; set; }
+            public int height { get; set; }
+            public int font_size { get; set; }
+            public string color { get; set; }
+            public string text { get; set; }
+            public int line_height { get; set; }
+            public string font_name { get; set; }
+        }
+
+        public class Game_Type_List
         {
             public string name { get; set; }
             public string value { get; set; }
         }
-
-        public class Result
-        {
-            public IList<Item> items { get; set; }
-            public IList<object> sort_filter_list { get; set; }
-            public IList<GameTypeList> game_type_list { get; set; }
-            public IList<object> time_range_list { get; set; }
-        }
     }
+
 }
